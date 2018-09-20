bd9fb5042a0cfdb237d312198920f5e84a8a35ba
aaaaaa2493 <aaaaaa2493@yandex.ru>
2018-09-20 20:37:32 +0300

    Field lowered to 9x9 as in the standard minesweeper

--- src/main.kt
+++ src/main.kt
@@ -1,15 +1,14 @@
 
 fun main(args: Array<String>) {
 
-    println(".X........")
-    println(".....X..X.")
-    println("....X.....")
-    println("......X...")
-    println("..X.......")
-    println("....X.....")
-    println("..X.......")
-    println("..X.......")
-    println("..........")
-    println("....X.....")
+    println(".X.......")
+    println(".....X..X")
+    println("....X....")
+    println("......X..")
+    println("..X......")
+    println("....X....")
+    println("..X......")
+    println("..X......")
+    println("......X..")
 
 }
