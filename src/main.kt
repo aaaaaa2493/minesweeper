7a015a579ee36ce44b93d6076adce42a607759e4
aaaaaa2493 <aaaaaa2493@yandex.ru>
2018-09-20 23:09:45 +0300

    Added generation of points

--- src/main.kt
+++ src/main.kt
@@ -1,14 +1,33 @@
+import java.util.*
 
 fun main(args: Array<String>) {
 
-    println(".X.......")
-    println(".....X..X")
-    println("....X....")
-    println("......X..")
-    println("..X......")
-    println("....X....")
-    println("..X......")
-    println("..X......")
-    println("......X..")
+    val fieldSizeX = 9
+    val fieldSizeY = 9
+    val pointsCount = 10
+
+    val field = CharArray(fieldSizeX * fieldSizeY) {'.'}
+
+    val random = Random()
+    var points = 0
+    while (points != pointsCount) {
+
+        val x = random.nextInt(fieldSizeX)
+        val y = random.nextInt(fieldSizeY)
+
+        if (field[x * fieldSizeY + y] == '.') {
+            field[x * fieldSizeY + y] = 'X'
+            points++
+        }
+
+    }
+
+    for ((index, value) in field.withIndex()) {
+        if (index % fieldSizeY == 0 && index != 0) {
+            println()
+        }
+
+        print(value)
+    }
 
 }
