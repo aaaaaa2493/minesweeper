27830ea407bcef2a1092d62f353a7ce8be65625b
aaaaaa2493 <aaaaaa2493@yandex.ru>
2018-09-20 17:03:06 +0300

    Print field to the console

---
+++ src/main.kt
@@ -0,0 +1,15 @@
+
+fun main(args: Array<String>) {
+
+    println(".X........")
+    println(".....X..X.")
+    println("....X.....")
+    println("......X...")
+    println("..X.......")
+    println("....X.....")
+    println("..X.......")
+    println("..X.......")
+    println("..........")
+    println("....X.....")
+
+}
